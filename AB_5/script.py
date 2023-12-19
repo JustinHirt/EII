a555c84281cfd4d44fb8b4ed5d41cce1481703f5
Justin Hirt <justin.hirt@icloud.com>
2023-12-19 09:34:38 +0100

    Aufgabe Änderung

--- AB_5/script.py
+++ AB_5/script.py
@@ -1,28 +1,24 @@
 def Aufgabe1(b, a):
     return not a and not b
 
-def Task2(a, c):
+def Aufgabe2(a, c):
     return a or not c
 
-def Task3():
+def Aufgabe3():
     return False
 
-def Task4(a):
+def Aufgabe4(a):
     return a
 
-def Task5(a ,b):
+def Aufgabe5(a ,b):
     return a and not b
 
-def Task6(a, c):
+def Aufgabe6(a, c):
     return a and c
 
-def Task7(a, b, c):
+def Aufgabe7(a, b, c):
     return not (a and b) or not c
 
-<<<<<<< HEAD
-def Task8(a, b, c):
-=======
 def Aufgabe8(a, b, c):
->>>>>>> 7953821624dcda661d326e6cc10b162685b19d4f
     return (a and b) and not c
-#Hey Justin
+#Hey Nils
